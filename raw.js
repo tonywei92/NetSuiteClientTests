39baf520faab77c990af03d91709920bb05eff98
Tony Song <tonywei92@gmail.com>
2018-08-13 18:43:41 +0700

    add raw support

--- raw.js
+++ raw.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 import axios from 'axios';
 import _ from 'lodash';
+import sleep from 'await-sleep'
+
 const reqMode = process.env.REQ_MODE;
 const Knex = require('knex');
 axios.defaults.baseURL = process.env.TARGET_BASE_URL;
@@ -20,16 +22,32 @@ if(!_.isNil(process.argv[2])){
 async function raw(param, query){
     let ids = [];
     let data = await knex.raw(query);
-    let promisearr = data[0].map(element => {
+    let coll = data[0];
+    // let promises = [];
+    // coll.forEach(element => {
+    //     console.log('push:', element.id);
+    //     promises.push(axios.post(reqMode),{id: element.id});
+    // });
+
+    // await axios.all(promises).then(function(results) {
+    //     results.forEach(function(response) {
+    //         console.log(response.value);
+    //         mainObject[response.identifier] = response.value;
+    //     })
+    // });
+
+    for(let i = 0; i<coll.length-1;i++){
         // console.log(element);
-        return await axios.post(reqMode,data).then(response =>{
-            console.log(response.data);
+        await sleep(500);
+        console.log(coll[i].id);
+        await axios.post(reqMode,{id: coll[i].id}).then(response =>{
+            console.log(response.data + ' - OK');
             return response;
         }).catch(error => {
             console.log(error.response.data);
             return error;
         })
-    });;
+    };
 
     await Promise.all(promisearr);
     process.exit();process.exit(0);
